b699f2972b8ab7d44cef3c1a6da6063255105ea9
Jan Rochel <jrochel@besport.com>
2017-08-11 15:25:43 +0200

    customised PhantomJS script

--- phantomjs-script.js
+++ phantomjs-script.js
@@ -1,13 +1,18 @@
-// Simple Javascript example
+var url = 'https://alpha.besport.com';
 var page = require('webpage').create();
-var url = 'http://phantomjs.org/';
 
-console.log('Loading a web page: ' + url);
+page.settings.userAgent = 'Mozilla/5.0 (usersim) Gecko/20100101 Firefox/54.0';
 
 page.open(url, function(status) {
-  var title = page.evaluate(function() {
-    return document.title;
-  });
-  console.log('Page title is: ' + title);
-  phantom.exit();
+	setInterval(function() {
+		console.log("URL: " + page.url);
+		// page.render('screenshot.png');
+    page.evaluate(function () {
+      var selector ='button:not(.sign-up-button):not(.sign-in-button), a:not(.topbar-logo):not(.topbar-btn):not(.logout-btn), .ot-car-ribbon-list-item';
+      var clickables = document.querySelectorAll(selector);
+      var random_index = [Math.floor(Math.random()*clickables.length)];
+      var clickable = clickables[random_index];
+      clickable.click();
+    });
+	}, 1000);
 });
