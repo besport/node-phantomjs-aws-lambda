0b93f4ce64b8537a4d1f52d48536002c0430d651
Jan Rochel <jrochel@besport.com>
2017-08-23 18:58:18 +0200

    ensure we do not browse other sites

--- phantomjs-script.js
+++ phantomjs-script.js
@@ -1,11 +1,20 @@
-var url = 'https://alpha.besport.com';
+var url = 'https://alpha.besport.com/';
 var page = require('webpage').create();
+
+function strStartsWith(str, prefix) {
+    return str.indexOf(prefix) === 0;
+}
 
 page.settings.userAgent = 'Mozilla/5.0 (usersim) Gecko/20100101 Firefox/54.0';
 
 page.open(url, function(status) {
 	setInterval(function() {
 		console.log("URL: " + page.url);
+		if (!strStartsWith(page.url, url))
+		{
+			console.log ("strayed to another page. exiting...")
+			phantom.exit();
+		};
 		// page.render('screenshot.png');
     page.evaluate(function () {
       var selector ='button:not(.sign-up-button):not(.sign-in-button):not(.logout-btn):not(.report-btn):not(.ot-drp-menu-itm), a:not(.topbar-logo):not(.topbar-btn):not([rel="nofollow"]), .ot-car-ribbon-list-item';
